134c3868cae7d1267945f7c351d52b34d79d4be3
MatheusHMafra <maframatheus982@gmail.com>
2023-12-05 19:22:31 -0300

    c

---
+++ js/scriptMulti.js
@@ -0,0 +1,16 @@
+/*
+Elementos
+*/
+const statusElement = document.getElementById('status');
+const liveElement = document.getElementById('live');
+
+// Multi stream elements
+if (window.location.href.includes('?channel=')) {
+    // Para cada canal apoós o ?channel= e nao pegar o resto da url
+    // Exemplo: ?channel=matheushmafra&channel=felps
+    for (let channel of window.location.href.split('?channel=')[1].split('&')) {
+        // Criar um iframe para cada canal
+        liveElement.innerHTML += `<iframe src="https://player.twitch.tv/?channel=${channel}&parent=matheushmafra.github.io" height="500px"
+        width="50%" frameborder="0"></iframe>`;
+    }
+}
